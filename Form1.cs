142a11ba752481c8a182902c78aca846b7dd9104
Lab2-12 <Lab2-12@LAB2-12>
2025-04-08 11:22:57 -0600

    ayh

--- Form1.cs
+++ Form1.cs
@@ -12,11 +12,30 @@ namespace Examen_P3_Rebeca
 {
     public partial class Form1 : Form
     {
+        Usuario Usu = new Usuario();
         public Form1()
         {
             InitializeComponent();
         }
 
-       
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            string user = txbUsuario.Text;
+            string pass = txbPassword.Text;
+
+            var lista = Usu.ObtenerUser();
+
+            var validar = lista.FirstOrDefault(u => u.User == user && u.Password == pass);
+            if (validar != null)
+            {
+                this.Hide();
+                Inicio inicio = new Inicio();
+                inicio.Show();
+            }
+            else
+            {
+                MessageBox.Show("Error critico");
+            }
+        }
     }
 }
---
+++ Inicio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_P3_Rebeca
+{
+    internal class Inicio : Form1
+    {
+        
+    }
+}
